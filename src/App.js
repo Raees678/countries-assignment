c6c474bec1e61bf96b81470341014d3767b4abb0
Raees Rajwani <raeesrajwani95@gmail.com>
2020-10-07 23:37:32 +0530

    Elevated search text state to top level, adds additional styling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import "./App.css";
 import SearchBox from "./SearchBox";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: "",
+    };
+
+    this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+  }
+
+  handleSearchTextChange(searchText) {
+    this.setState({
+      searchText: searchText,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="working-area">
-          <SearchBox></SearchBox>
+          <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&display=swap" rel="stylesheet" />
+
+          <div id="title">Country Search</div>
+          <SearchBox searchText={this.state.searchText} onSearchTextChange={this.handleSearchTextChange}></SearchBox>
+          <div>{this.state.searchText}</div>
         </div>
       </div>
     );
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -4,13 +4,23 @@ import "./SearchBox.css";
 class SearchBox extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleSearchTextChange = this.handleSearchTextChange.bind(this);
+  }
+
+  handleSearchTextChange(event) {
+    this.props.onSearchTextChange(event.target.value);
   }
 
   render() {
     return (
       <div id="search-box">
         <form id="search-box-form-div">
-          <input id="search-box-form-div-input" placeholder="Enter a country to get started"></input>
+          <input
+            id="search-box-form-div-input"
+            placeholder="Enter a country to get started"
+            onChange={this.handleSearchTextChange}
+          ></input>
         </form>
       </div>
     );
